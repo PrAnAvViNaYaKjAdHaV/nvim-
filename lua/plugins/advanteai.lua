faf49bef2553eb9be976e107e60ae08b5f7e056a
PrAnAvViNaYaKjAdHaV <pj8841466@gmail.com>
2025-01-27 09:04:07 +0530

    updated the advante nvim and obsidian nvim

--- lua/plugins/advanteai.lua
+++ lua/plugins/advanteai.lua
@@ -5,6 +5,15 @@ return {
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
     -- add any opts here
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        __inherited_from = "openai",
+        api_key_name = "",
+        endpoint = "http://127.0.0.1:11434/v1",
+        model = "deepseek-r1:latest",
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
